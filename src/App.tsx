b4ace7a2a877cc44a53d03b8383a7e03673f8c46
Danila <syvs0990@mail.ru>
2022-03-23 14:14:11 +0700

    Connected fetch API

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,29 @@ import Head from "./Head"
 import News from "./News"
 import Guide from "./Guide";
 
+
 function App() {
+    fetch('http://localhost:8080/api/login?username=test@mail.com&password=test', {
+        method: "POST"
+    })
+    .then(response => {
+        console.log(response)
+        return response.json()
+    })
+    .then(data => {
+        console.log(data);
+        
+    })
+
+    fetch("http://localhost:8080/api/v1/user?id=00000000-0000-0000-0005-000000000001")
+    .then(response => {
+        console.log(response);
+        return response.json()
+    })
+    .then(data => {
+        console.log(data);
+        
+    })
 
     return (
         <>  
